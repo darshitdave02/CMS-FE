a19eb18b090fde1ec84bb421ba4edccceeeef3d7
Darshit Dave <darshitdave02@gmail.com>
2023-03-10 11:10:35 +0530

    feat: make request from modal form to add an entry

--- src/components/ModalForm/index.jsx
+++ src/components/ModalForm/index.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import './modalForm.css';
 import PropTypes from 'prop-types';
-import  makeRequest  from '../../utils/makeRequest';
+import makeRequest from '../../utils/makeRequest';
 import { BACKEND_URL } from '../../constants/apiEndPoints';
 
 export default function ModalForm(props) {
   const [fields, setFields] = useState([]);
-  // const [error, setError] = useState(null);
+  const [formData, setFormData] = useState({});
 
   useEffect(() => {
     const token = localStorage.getItem('token');
@@ -19,7 +19,6 @@ export default function ModalForm(props) {
             url: `/collections/fields/${props.collectionName}`,
             method: 'POST',
             headers: {
-              // 'Referrer-Policy': 'strict-origin-when-cross-origin',
               Authorization: `Bearer ${token}`,
             },
           },
@@ -27,6 +26,12 @@ export default function ModalForm(props) {
           null // pass null as navigate parameter if you don't want to navigate to an error page
         );
         if (response.status === 200) {
+          // Initialize formData state with empty values for each field
+          const initialFormData = {};
+          response.data.forEach((field) => {
+            initialFormData[field] = '';
+          });
+          setFormData(initialFormData);
           setFields(response.data);
         } else {
           console.log(response.message);
@@ -38,6 +43,45 @@ export default function ModalForm(props) {
     }
   }, [props.collectionName]);
 
+  const handleInputChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+    console.log(formData);
+  };
+
+  const handleAddClick = () => {
+    //const formDataJson = JSON.stringify(formData);
+    console.log(formData);
+    const token = localStorage.getItem('token');
+    try {
+      const fetchData = async () => {
+        const response = await makeRequest(
+          BACKEND_URL,
+          {
+            url: `collections/add/${props.collectionName}`,
+            method: 'POST',
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          },
+          { data: formData },
+          null
+        );
+        if (response.status === 200) {
+          console.log('try ', response.message);
+        } else {
+          console.log('error ', response.message);
+        }
+      };
+      fetchData();
+    } catch (error) {
+      console.log('catch ', error);
+    }
+    props.onClose();
+  };
+
   if (!props.show) {
     return null;
   }
@@ -45,13 +89,18 @@ export default function ModalForm(props) {
     <div className="modal" onClick={props.onClose}>
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
         <div className="modal-header">
-          <span className="modal-title">New Company Profile</span>
+          <span className="modal-title">New {props.collectionName}</span>
         </div>
         <div className="modal-body">
           {fields.map((field, idx) => (
             <div key={idx}>
               <span>{field}</span>
-              <input type="text" />
+              <input
+                type="text"
+                name={field}
+                value={formData[field]}
+                onChange={handleInputChange}
+              />
             </div>
           ))}
         </div>
@@ -59,7 +108,9 @@ export default function ModalForm(props) {
           <button onClick={props.onClose} className="modal-close-button">
             Close
           </button>
-          <button className="modal-Add-button">Add</button>
+          <button onClick={handleAddClick} className="modal-Add-button">
+            Add
+          </button>
         </div>
       </div>
     </div>
